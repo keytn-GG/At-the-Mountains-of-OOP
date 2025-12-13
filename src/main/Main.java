14f87a559b8fed4b78aaeaaf192ef652722553cb
kentaro <talcos.go.to.nexus@gmail.com>
2025-12-13 23:20:14 +0900

    error: Character.javaでGameMasterをnewしている問題。責務分けを検討中。

--- src/main/Main.java
+++ src/main/Main.java
@@ -22,6 +22,6 @@ public class Main {
         CharacterBuilder builder = new CharacterBuilder();
         Character player = builder.build(spec);
 
-        player.showStatus();
+//        player.showStatus();
     }
 }
