0f17e3706b39c9152f966fa975b5063121574a42
kentaro <talcos.go.to.nexus@gmail.com>
2025-12-14 12:20:57 +0900

    update: StatusViewScene.javaにjavadocを追加。

--- src/scene/StatusViewScene.java
+++ src/scene/StatusViewScene.java
@@ -4,15 +4,27 @@ import character.Character;
 import game.GameContext;
 import view.CharacterStatusView;
 
+/**
+ * プレーヤーキャラクターのステータスを表示するシーン
+ */
 public class StatusViewScene implements Scene {
     private final GameContext ctx;
     private final CharacterStatusView view;
     
+    /**
+     * コンストラクタ
+     * @param ctx シーンに渡されるデータをまとめたもの -> GameContext
+     */
     public StatusViewScene(GameContext ctx) {
         this.ctx = ctx;
         this.view = new CharacterStatusView(ctx.getGm());
     }
     
+    /**
+     * このシーンのplay()
+     * ctxからplayerを取得して、CharacterStatusViewクラスのshow()を利用する
+     * 次のSceneをreturnする -> 直前のシーンに戻る処理を入れる予定
+     */
     public Scene play() {
         Character player = ctx.getPlayer();
         
