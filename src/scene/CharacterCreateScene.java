fcbc5b353b3de7c3631814797d4b9995972c38c0
kentaro <talcos.go.to.nexus@gmail.com>
2025-12-14 12:34:55 +0900

    update: CharacterCreateScene.javaを調整。

--- src/scene/CharacterCreateScene.java
+++ src/scene/CharacterCreateScene.java
@@ -4,18 +4,47 @@ import character.Character;
 import character.CharacterBuilder;
 import character.CharacterSpec;
 import character.CharacterUI;
+import game.GameContext;
+import gamemaster.GameMaster;
 
 /**
  * キャラクターを作るシーン
  */
 public class CharacterCreateScene implements Scene {
-    // キャラクターのデータを作る
-    CharacterUI ui = new CharacterUI(sc, gm);
-    CharacterSpec spec = ui.createSpec();
+    private final GameContext ctx;
+    private final GameMaster gm;
 
-    // キャラクターを組み立てる
-    CharacterBuilder builder = new CharacterBuilder();
-    Character player = builder.build(spec);
-    
-    public Scene play();
+    /**
+     * コンストラクタ
+     * @param ctx シーンに渡されるデータをまとめたもの -> GameContext
+     */
+    public CharacterCreateScene(GameContext ctx) {
+        this.ctx = ctx;
+        this.gm = ctx.getGm();
+    }
+
+    /**
+     * キャラクターを作るシーン
+     * 1. UIにctxのscとgmを渡す
+     * 2. 返ってきたSpecを格納
+     * 3. Builderをインスタンス化
+     * 4. Builderのbuild()に、specを渡す
+     * 5. 返ってきたキャラクターを格納
+     * 6. キャラクターをctxのplayerに格納
+     * 7. 次のシーンにctxを渡しながらインスタンス化する
+     */
+    public Scene play() {
+        // キャラクターのデータを作る
+        CharacterUI ui = new CharacterUI(ctx.getSc(), gm);
+        CharacterSpec spec = ui.createSpec();
+
+        // キャラクターを組み立てる
+        CharacterBuilder builder = new CharacterBuilder();
+        Character player = builder.build(spec);
+
+        // 作ったキャラクターをGameContextにセットする
+        ctx.setPlayer(player);
+
+        return new StatusViewScene(ctx);
+    };
 }
